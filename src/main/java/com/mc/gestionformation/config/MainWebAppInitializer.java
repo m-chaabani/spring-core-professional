77b007161c6c3e2dec4680b1317f0fe068c57e3e
m-chaabani <montacer.chaabani.data@gmail.com>
2022-11-12 01:55:24 +0100

    MainWebAppInitializer to get rid of WEB.XML and Get Rid of XML WebApp Context

--- src/main/java/com/mc/gestionformation/config/MainWebAppInitializer.java
+++ src/main/java/com/mc/gestionformation/config/MainWebAppInitializer.java
@@ -2,16 +2,28 @@ package com.mc.gestionformation.config;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
-public class MainWebAppInitializer implements WebApplicationInitializer{
+// la classe qui remplace le Fichier WEB.XML : à partir de la version Servlet 3.0+
+public class MainWebAppInitializer implements WebApplicationInitializer {
 
 	@Override
-	public void onStartup(ServletContext servletContext) throws ServletException {
-		// TODO Auto-generated method stub
+	public void onStartup(final ServletContext sc) throws ServletException { 
 		
-	}
-	
+		System.out.println("Sans WEB.XML");
+		 AnnotationConfigWebApplicationContext root = new     
+		                             AnnotationConfigWebApplicationContext(); 
+		root.scan("com.mc"); 
+		sc.addListener(new ContextLoaderListener(root)); 
+		ServletRegistration.Dynamic appServlet = sc.addServlet("mvc", new DispatcherServlet(new GenericWebApplicationContext())); appServlet.setLoadOnStartup(1); 
+		appServlet.addMapping("/"); 
+		} 
+
 
 }
