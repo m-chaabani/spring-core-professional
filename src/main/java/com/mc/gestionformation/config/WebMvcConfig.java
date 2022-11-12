aea3816763ea533b3f423974366ef9db392d70e2
m-chaabani <montacer.chaabani.data@gmail.com>
2022-11-12 02:06:13 +0100

    ViewResolvers

--- src/main/java/com/mc/gestionformation/config/WebMvcConfig.java
+++ src/main/java/com/mc/gestionformation/config/WebMvcConfig.java
@@ -1,11 +1,138 @@
 package com.mc.gestionformation.config;
 
+import javax.servlet.ServletContext;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.context.ServletContextAware;
+import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.BeanNameViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.ResourceBundleViewResolver;
+import org.springframework.web.servlet.view.XmlViewResolver;
+import org.springframework.web.util.UrlPathHelper;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @Configuration
 @EnableWebMvc
-public class WebMvcConfig implements WebMvcConfigurer{
+public class WebMvcConfig implements WebMvcConfigurer, ServletContextAware {
+
+	ServletContext servletContext;
+
+	@Override
+	public void setServletContext(ServletContext servletContext) {
+		this.servletContext = servletContext;
+
+	}
+
+//	@Override
+//	public void addViewControllers(ViewControllerRegistry registry) {
+//		registry.addViewController("/").setViewName("index");
+
+	View v;
+//
+//	}
+
+	@Override
+	public void configurePathMatch(PathMatchConfigurer configurer) {
+		UrlPathHelper matrixPathHelper = new UrlPathHelper();
+		matrixPathHelper.setRemoveSemicolonContent(false);
+		configurer.setUrlPathHelper(matrixPathHelper);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+	}
+
+	@Bean
+	InternalResourceViewResolver viewResolver() {
+
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		viewResolver.setOrder(4);
+		JstlView j;
+		return viewResolver;
+
+	}
+
+	ResourceBundleViewResolver rsViewRsolver() {
+		ResourceBundleViewResolver rs = new ResourceBundleViewResolver();
+		rs.setBasename("spring-views");
+		rs.setOrder(3);
+		return rs;
+
+	}
+
+	XmlViewResolver xmlViewResolver() {
+		XmlViewResolver xmlViewResolver = new XmlViewResolver();
+		xmlViewResolver.setOrder(2);
+		return xmlViewResolver;
+
+	}
+
+	BeanNameViewResolver beanNameViewResolver() {
+		BeanNameViewResolver beanNameViewResolver = new BeanNameViewResolver();
+		beanNameViewResolver.setOrder(1);
+		return beanNameViewResolver;
+	}
+
+	@Bean("homePage")
+	View homePage() {
+		return new JstlView("/WEB-INF/views/acceuil.jsp");
+	}
+
+	@Bean
+	@Description("Thymeleaf Template Resolver")
+	public ServletContextTemplateResolver templateResolver() {
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setPrefix("/WEB-INF/views/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode("HTML5");
+		return templateResolver;
+	}
+
+	
+	@Bean
+	@Description("Spring Message Resolver")
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("i18n/messages");
+		return messageSource;
+	}
+	
+	
+	@Bean
+	@Description("Thymeleaf Template Engine")
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver());
+		 templateEngine.setTemplateEngineMessageSource(messageSource());
+		return templateEngine;
+	}
+
+	@Bean
+	@Description("Thymeleaf View Resolver")
+	public ThymeleafViewResolver ThymeleafViewResolver() {
+
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(templateEngine());
+		viewResolver.setOrder(0);
+		return viewResolver;
+
+	}
+
+
 
 }
